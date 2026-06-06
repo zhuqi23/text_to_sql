10f8ee7b2b3c69362973b3229301afeaa64e21ae
mygit <3113961297@qq.com>
2026-06-07 00:12:22 +0800

    创建项目并连接数据库

---
+++ src/main/java/com/text_to_sql/text_to_sql/TextToSqlApplication.java
@@ -0,0 +1,13 @@
+package com.text_to_sql.text_to_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/text_to_sql/text_to_sql/TextToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.text_to_sql.text_to_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
